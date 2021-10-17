9a78cc95a48a0501034971d93debe463d6e9d753
Ofek Harpaz <50238704+ofekharpaz@users.noreply.github.com>
2021-10-17 12:43:14 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(787, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "התחל";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(787, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(23, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(787, 142);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(23, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(45, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(542, 340);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(816, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "גודל הסידרה";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(816, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "גודל הלוח";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(590, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "פגיעה";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(656, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "בול";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(787, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "ניחוש הצבעים";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(787, 258);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "סידור";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(706, 25);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(62, 338);
+            this.panel2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(704, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "כל הצבעים";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(593, 23);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(89, 340);
+            this.panel3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 385);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,506 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int buttonhigh;
+        Button[,] Guesses;
+        Button[,] BolPgia;
+        Button[] ColorArray;
+        TextBox[,] bolpgia;
+        int[] options;
+        int num = 1;
+        int numOfBol = 0;
+        Color y;
+        int[][] sets;
+        int[][] temp;
+        int numOfSets;
+        bool alreadyOk = false;
+        int shura3 = 0;
+        int sidra;
+        int permutNum;
+        int[][] all;
+        int[] tmp;
+        int[][] temp2;
+        int[] saveguess;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int comb2(int n, int k)
+        {
+            int nsum = factorial(n);
+            int ksum = factorial(k);
+            return nsum/(ksum*(factorial(n-k)));
+        }      
+        private int[][] generateArr()  //sets יוצר את המערך 
+        {
+            int numOfSets = comb2(10, Guesses.GetLength(1));
+            int[][] sets = new int[numOfSets][];
+            for (int g = 0; g < numOfSets; g++)
+            {
+                sets[g] = new int[Guesses.GetLength(1)];
+            }
+            return sets;
+        }
+        private int[][] getAllSubSets(int[] arr)  //את כל הסדרות האפשריות setsמכניס ל 
+        {
+            int[][] sets = generateArr();
+            int[] indexes = new int[Guesses.GetLength(1)];
+            for (int i = 0; i < Guesses.GetLength(1); i++)
+            {
+                indexes[i] = i;
+            }
+            bool finished = false;
+            bool found = false;
+            int count = 0;
+            while (!finished)
+            {
+                for (int t = 0; t < Guesses.GetLength(1); t++)
+                {
+                    sets[count][t] = arr[indexes[t]];
+                }
+                int j = Guesses.GetLength(1) - 1;
+                int i = 1;
+                found = false;
+                while (j >= 0 && !found)
+                {
+                    if (indexes[j] < 10 - i)
+                    {
+                        found = true;
+                        indexes[j]++;
+                        for (int k = j + 1; k < Guesses.GetLength(1); k++)
+                        {
+                            indexes[k] = indexes[j] + k - j;
+                        }
+                    }
+                    else
+                    {
+                        j--;
+                        i++;
+                    }
+                }
+                count++;
+                if (!found)
+                    finished = true;
+            }
+
+            return sets;
+        }
+        private void PrintArr(int[] arr) // מדפיס את המערך
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 1)
+                    Guesses[shura3, i].BackColor = Color.Blue;
+                else if (arr[i] == 2)
+                    Guesses[shura3, i].BackColor = Color.Green;
+                else if (arr[i] == 3)
+                    Guesses[shura3, i].BackColor = Color.Black;
+                else if (arr[i] == 4)
+                    Guesses[shura3, i].BackColor = Color.Yellow;
+                else if (arr[i] == 5)
+                    Guesses[shura3, i].BackColor = Color.Red;
+                else if (arr[i] == 6)
+                    Guesses[shura3, i].BackColor = Color.Purple;
+                else if (arr[i] == 7)
+                    Guesses[shura3, i].BackColor = Color.Brown;
+                else if (arr[i] == 8)
+                    Guesses[shura3, i].BackColor = Color.Pink;
+                else if (arr[i] == 9)
+                    Guesses[shura3, i].BackColor = Color.Cyan;
+                else if (arr[i] == 10)
+                    Guesses[shura3, i].BackColor = Color.Coral;
+            }
+        }
+        private int[] clone(int[] x)  //משכפל את המערך
+        {
+            if (x == null)
+                return null;
+            int[] clone = new int[Guesses.GetLength(1)];
+            for (int i = 0; i < Guesses.GetLength(1); i++)
+            {
+                clone[i] = x[i];
+            }
+            return clone;
+        }  
+        private bool inSet(int[] s, int num) //משווה בין שני ערכים
+        {
+            for (int i = 0; i < Guesses.GetLength(1); i++)
+            {
+                if (s[i] == num)
+                    return true;
+            }
+            return false;
+        }
+        private void removeBadSets(int[][] sets, int[] guess, int numCorrect, int numOfSets) //מוציא את הסדרות שלא יכולות להיות
+        {
+            for (int i = 0; i < numOfSets; i++)
+            {
+                if (sets[i] != null)
+                {
+                    int count = 0;
+                    for (int j = 0; j < Guesses.GetLength(1); j++)
+                    {
+                        if (inSet(sets[i], guess[j]))
+                        {
+                            count++;
+                        }
+                    }
+                    if (count > numCorrect)
+                    {
+                        sets[i] = null;
+                    }
+                }
+            }
+        }
+        private int[] getFirstAvailable(int[][] sets, int numOfSets)//null מחזיר את הערך הראשון שאינו 
+        {
+            for (int i = 0; i < numOfSets; i++)
+            {
+                if (sets[i] != null)
+                {
+                    return sets[i];
+                }
+            }
+            return null;
+        }
+        private int[] guessSet(int[][] sets, int numOfSets, ref bool alreadyOk)   //מנחש את הסדרה
+        {
+            int p = int.Parse(bolpgia[shura3, 0].Text);
+            int b = int.Parse(bolpgia[shura3, 1].Text);
+            if (p + b == Guesses.GetLength(1))
+            {
+                button2.Enabled = false;
+                return saveguess;
+            }
+            removeBadSets(sets, saveguess, b + p, numOfSets);
+            int[] guess = clone(getFirstAvailable(sets, numOfSets));
+            saveguess = guess;
+            shura3++;
+            printSet(guess);
+            if (b == Guesses.GetLength(1))
+                alreadyOk = true;
+            return guess;        
+        }
+        private void printSet(int[] set) // מדפיס את המערך
+        {
+            if (shura3 < Guesses.GetLength(0))
+            {
+                for (int i = 0; i < Guesses.GetLength(1); i++)
+                {
+                    if (set[i] == 1)
+                        Guesses[shura3, i].BackColor = Color.Blue;
+                    else if (set[i] == 2)
+                        Guesses[shura3, i].BackColor = Color.Green;
+                    else if (set[i] == 3)
+                        Guesses[shura3, i].BackColor = Color.Black;
+                    else if (set[i] == 4)
+                        Guesses[shura3, i].BackColor = Color.Yellow;
+                    else if (set[i] == 5)
+                        Guesses[shura3, i].BackColor = Color.Red;
+                    else if (set[i] == 6)
+                        Guesses[shura3, i].BackColor = Color.Purple;
+                    else if (set[i] == 7)
+                        Guesses[shura3, i].BackColor = Color.Brown;
+                    else if (set[i] == 8)
+                        Guesses[shura3, i].BackColor = Color.Pink;
+                    else if (set[i] == 9)
+                        Guesses[shura3, i].BackColor = Color.Cyan;
+                    else if (set[i] == 10)
+                        Guesses[shura3, i].BackColor = Color.Coral;
+                }
+            }
+        }
+        private int factorial(int k) //מחשב עצרת
+        {                     
+            int factorial = 1;
+            for (int i = k; i > 0; i--)
+            {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+        private void Permut(int[] permut, int[][] all)//את כל הסדרות האפשריות all מכניס למערך 
+        {
+            int[] indexes = new int[Guesses.GetLength(1)];
+            for (int r = 0; r < Guesses.GetLength(1); r++)
+            {
+                indexes[r] = 0;
+            }
+
+            int i = 1;
+            int j, k;
+            k = 0;
+            int count = 0;
+            while (i < Guesses.GetLength(1))
+            {
+                if (indexes[i] < i)
+                {
+                    j = i % 2 * indexes[i];
+                    // swap - k as tmp    
+                    k = permut[j];
+                    permut[j] = permut[i];
+                    permut[i] = k;
+                    all[count] = clone(permut);
+
+                    count++;
+                    indexes[i]++;
+                    i = 1;
+                }
+                else
+                {
+                    indexes[i] = 0;
+                    i++;
+                }
+           }
+
+        }
+        private void removeBadPerms(int[][] perms, int[] guess, int numCorrect, int numPerms)//מוציא את הסדרות שלא יכולות להיות
+        {
+            for (int i = 0; i < numPerms; i++)
+            {
+                if (perms[i] != null)
+                {
+                    int count = 0;
+                    for (int j = 0; j < Guesses.GetLength(1); j++)
+                    {
+                        if (perms[i][j] == guess[j])
+                            count++;
+                    }
+                    if (count > numCorrect)
+                    {
+                        perms[i] = null;
+                    }
+                }
+
+            }
+        }
+        private int[] guessPerm(int[][] perms, int numPerms)//מנחש את הסדרה
+        {
+            int b = int.Parse(bolpgia[shura3, 1].Text);
+
+            if (b == Guesses.GetLength(1))
+            {
+                bolpgia[shura3 + 1, 1].Enabled = false;
+                Win();          
+                return saveguess;
+            }
+            removeBadPerms(perms, saveguess, b, numPerms);
+            int[] guess = clone(getFirstAvailable(perms, numPerms));
+            saveguess = guess;
+            shura3++;
+            printSet(guess);
+            return guess;
+        }
+        private void Win()
+        {
+            MessageBox.Show("i won :D");
+            button3.Enabled = false;
+        }
+        private void button1_Click(object sender, EventArgs e)// יוצר את הלוח
+        {
+            try
+            {
+                int sidra = int.Parse(textBox1.Text);
+                if (sidra > 10)
+                    MessageBox.Show("to many colors");
+                else
+                {
+                    int i;
+                    panel1.Visible = true;
+
+                    Guesses = new Button[int.Parse(textBox2.Text), sidra];
+                    ColorArray = new Button[10];
+                    bolpgia = new TextBox[Guesses.GetLength(0), 2];
+                    int num = 0;
+                    buttonhigh = 30;
+                    panel1.Height = buttonhigh * Guesses.GetLength(0);
+                    panel3.Height = buttonhigh * bolpgia.GetLength(0);
+                    for (int j = 0; j < Guesses.GetLength(0); j++)
+                    {
+                        for (i = 0; i < Guesses.GetLength(1); i++)
+                        {
+                            Guesses[j, i] = new Button();
+                            Guesses[j, i].Width = (panel1.Width / Guesses.GetLength(1));
+                            Guesses[j, i].Height = buttonhigh;
+                            Guesses[j, i].Location = new Point(Guesses[j, i].Width * i, Guesses[j, i].Height * j);
+                            Guesses[j, i].Tag = num.ToString();
+                            num++;
+                            panel1.Controls.Add(Guesses[j, i]);
+                        }
+                    }
+                    num = 0;
+                    for (int j = 0; j < bolpgia.GetLength(0); j++)
+                    {
+                        for (i = 0; i < bolpgia.GetLength(1); i++)
+                        {
+                            bolpgia[j, i] = new TextBox();
+                            bolpgia[j, i].Width = (panel3.Width / bolpgia.GetLength(1));
+                            bolpgia[j, i].Location = new Point(bolpgia[j, i].Width * i, (bolpgia[j, i].Height * j)+(Guesses[j, i].Height * j-bolpgia[j, i].Height * j));
+                            bolpgia[j, i].Tag = num.ToString();
+                            num++;
+                            panel3.Controls.Add(bolpgia[j, i]);
+                        }
+                    }
+                    num = 0;
+                    for (int j = 0; j < Guesses.GetLength(0); j++)
+                        for (i = 0; i < Guesses.GetLength(1); i++)
+                        {
+                            Guesses[j, i].Enabled = false;
+                        }
+                    y = button1.BackColor;
+                    int[] options = new int[10];
+                    for (i = 1; i < 10 + 1; i++)
+                    {
+                        options[i - 1] = i;
+                    }
+                    int[][] sets = getAllSubSets(options);
+                    numOfSets = comb2(10, Guesses.GetLength(1));
+                    int d = factorial(10);
+                    PrintArr(sets[0]);
+                    saveguess = clone(sets[0]);
+                    temp = sets;
+                    button1.Enabled = false;
+                    button3.Enabled = false;
+                    button2.Enabled = true;
+                   
+                    textBox1.Enabled = false;
+                    textBox2.Enabled = false;
+                    for (i = 0; i < bolpgia.GetLength(0); i++)
+                        for (int j = 0; j < bolpgia.GetLength(1); j++)
+                            bolpgia[i, j].Enabled = false;
+                    for (i = 0; i < bolpgia.GetLength(1); i++)
+                        bolpgia[shura3, i].Enabled = true;
+                    if (panel2.Height > panel1.Height)
+                        this.Height = panel2.Height + 80;
+                    else
+                        this.Height = panel1.Height + 80;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("not valid numbers");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)//מנחש את הסדרה
+        {
+            try
+            {
+                
+                int p = int.Parse(bolpgia[shura3,0].Text);
+                int b = int.Parse(bolpgia[shura3, 1].Text);
+                if (p + b > Guesses.GetLength(1))
+                    MessageBox.Show("too many bools or pgia");
+                else
+                {
+                    for (int i = 0; i < bolpgia.GetLength(0); i++)
+                        for (int j = 0; j < bolpgia.GetLength(1); j++)
+                            bolpgia[i, j].Enabled = false;
+                    for (int i = 0; i < bolpgia.GetLength(1); i++)
+                    {
+                        if (shura3 != Guesses.GetLength(0))                           
+                            bolpgia[shura3 + 1, i].Enabled = true;
+                    }
+                    int[] rightSet = guessSet(temp, numOfSets, ref alreadyOk);
+                    if (p + b == Guesses.GetLength(1))
+                    {
+                        if (alreadyOk)
+                            Win();
+                        else
+                        {
+                            for (int i = shura3; i < bolpgia.GetLength(0); i++)
+                                bolpgia[i, 0].Enabled = false;
+                            permutNum = factorial(Guesses.GetLength(1));
+                            all = new int[permutNum][];
+                            for (int i = 0; i < permutNum; i++)
+                            {
+                                all[i] = new int[Guesses.GetLength(1)];
+                            }
+                            tmp = clone(rightSet);
+                            if (tmp != null)
+                                Permut(tmp, all);
+                            all[permutNum - 1] = rightSet;
+                            temp2 = all;
+                            button3.Enabled = true;
+                            saveguess = rightSet;
+                            int[] correct = guessPerm(temp2, permutNum);        
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("invalid");
+            }
+
+            }
+        private void button3_Click(object sender, EventArgs e)// מסדר את הסדרה
+        {
+            try
+            {
+                int b = int.Parse(bolpgia[shura3, 1].Text);
+                if (b > Guesses.GetLength(1))
+                    MessageBox.Show("too many bools");
+                else
+                {
+                    bolpgia[shura3, 1].Enabled = false;
+                    bolpgia[shura3+1, 1].Enabled = true;
+                    temp2 = all;
+                    int[] correct = guessPerm(temp2, permutNum);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("invalid number of bools");
+            }
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ColorArray = new Button[10];
+            int i;
+            int num = 0;
+            for (i = 0; i < ColorArray.Length; i++)
+            {
+                ColorArray[i] = new Button();
+                ColorArray[i].Width = panel2.Width;
+                ColorArray[i].Height = panel2.Height / ColorArray.Length;
+                ColorArray[i].Location = new Point(0, ColorArray[i].Height * i);
+                ColorArray[i].Tag = num.ToString();
+                num++;
+                panel2.Controls.Add(ColorArray[i]);
+            }
+            ColorArray[0].BackColor = Color.Blue;
+            ColorArray[1].BackColor = Color.Green;
+            ColorArray[2].BackColor = Color.Black;
+            ColorArray[3].BackColor = Color.Yellow;
+            ColorArray[4].BackColor = Color.Red;
+            ColorArray[5].BackColor = Color.Purple;
+            ColorArray[6].BackColor = Color.Brown;
+            ColorArray[7].BackColor = Color.Pink;
+            ColorArray[8].BackColor = Color.Cyan;
+            ColorArray[9].BackColor = Color.Coral;
+           
+            for (i = 0; i < ColorArray.Length; i++)
+            {
+                ColorArray[i].Enabled = false;
+            }
+            button2.Enabled = false;
+            button3.Enabled = false;
+            
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
